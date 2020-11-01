81dd2a0adfce089c974917b192c0481dcae7d5ed
Sneez <ecstanto@ucsc.edu>
2020-11-01 01:31:25 -0700

    added spinner, refactored getweather func

--- src/App.vue
+++ src/App.vue
@@ -10,38 +10,42 @@
         type="text" />
     </div>
 
-    <!-- city and weather description -->
-    <h4 class="top-padding">{{ focusedWeather.city }}</h4>
-    <h2>{{ focusedWeather.description }}</h2>
+    <div v-if="loaded">
 
-    <!-- info bar with temp and wind -->
-    <div class="row info-row" v-if="focusedWeather.temp !== ''">
-      <div class="col-4">
-        <h3 class="gold-color">{{ focusedWeather.temp }}&#176;F</h3>
-      </div>
-      <div class="col-8">
-        <h3 class="no-bottom-margin right-float">
-          <span>{{ focusedWeather.wind }} mph </span>
-          <img class="small-icon" src="./assets/icons/Windy.svg">
-        </h3>
-      </div>
-    </div>
+      <!-- city and weather description -->
+      <h4 class="top-padding">{{ city }}</h4>
+      <h2>{{ focusedWeather.description }}</h2>
 
-    <!-- weather icon -->
-    <img v-if="focusedWeather.icon !== ''" class="icon-style" :src="getImgUrl(focusedWeather.icon)">
-
-    <!-- forecast divs -->
-    <div class="row">
-      <div class="forecast-container">
-        <div v-for="day in forecast" :key="day.date" class="forecast">
-          <div>{{day.date}}</div>
-          <div class="gold-color">{{day.temp}}&#176;F</div>
-          <img class="forecast-icon" :src="getImgUrl(day.icon)">
-          <div class="small-text">{{day.wind}} mph</div>
+      <!-- info bar with temp and wind -->
+      <div class="row info-row" v-if="focusedWeather.temp !== ''">
+        <div class="col-4">
+          <h3 class="gold-color">{{ focusedWeather.temp }}&#176;F</h3>
+        </div>
+        <div class="col-8">
+          <h3 class="no-bottom-margin right-float">
+            <span>{{ focusedWeather.wind }} mph </span>
+            <img class="small-icon" src="./assets/icons/Windy.svg">
+          </h3>
         </div>
       </div>
-    </div>
 
+      <!-- weather icon -->
+      <img v-if="focusedWeather.icon !== ''" class="icon-style" :src="getImgUrl(focusedWeather.icon)">
+
+      <!-- forecast divs -->
+      <div class="row">
+        <div class="forecast-container">
+          <div v-for="day in forecast" :key="day.date" class="forecast">
+            <div>{{day.date}}</div>
+            <div class="gold-color">{{day.temp}}&#176;F</div>
+            <img class="forecast-icon" :src="getImgUrl(day.icon)">
+            <div class="small-text">{{day.wind}} mph</div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <div v-if="!loaded" class="loader"></div>
   </div>
 </template>
 
@@ -54,11 +58,12 @@ export default {
         temp: '',
         wind: '',
         description: '',
-        city: '',
         icon: '',
         date: ''
       },
-      forecast: []
+      city: '',
+      forecast: [],
+      loaded: false
     }
   },
 
@@ -72,12 +77,11 @@ export default {
 
     // listener for setting current city and weather after autocomplete is clicked
     this.autocomplete.addListener('place_changed', () => {
-      this.forecast = [];
       let place = this.autocomplete.getPlace();
       let ac = place.address_components;
       let lat = place.geometry.location.lat();
       let lon = place.geometry.location.lng();
-      this.focusedWeather.city = ac[0]["short_name"];
+      this.city = ac[0]["short_name"];
       this.getWeather(lat, lon);
     });
 
@@ -102,6 +106,7 @@ export default {
     async getWeather(lat, lon){
 
       try{
+        this.loaded = false;
         let request = 'http://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+
           '&exclude=minutely,hourly,alerts&appid='+process.env.VUE_APP_WEATHER+'&units=imperial';
         const res = await fetch(request);
@@ -112,21 +117,25 @@ export default {
         } else {
 
           // set focused weather data
-          this.focusedWeather.temp = Math.round(data.current.temp);
-          this.focusedWeather.wind = Math.round(data.current.wind_speed);
-          this.focusedWeather.date = this.convertDate(data.current.dt);
-          this.focusedWeather.description = this.capitalizeWords(data.current.weather[0].description);
-          this.focusedWeather.icon = this.getIconName(data.current.weather[0].icon);
+          this.focusedWeather = {
+            temp: Math.round(data.current.temp),
+            wind: Math.round(data.current.wind_speed),
+            date: this.convertDate(data.current.dt),
+            description: this.capitalizeWords(data.current.weather[0].description),
+            icon: this.getIconName(data.current.weather[0].icon)
+          }
 
           // set forecast array
-          for(let i = 0; i < 8; i++){
-            this.forecast.push({
-              temp: Math.round(data.daily[i].temp.day),
-              wind: Math.round(data.daily[i].wind_speed),
-              icon: this.getIconName(data.daily[i].weather[0].icon),
-              date: this.convertDate(data.daily[i].dt)
-            })
-          }
+          this.forecast = data.daily.map((day) => {
+            return {
+              temp: Math.round(day.temp.day),
+              wind: Math.round(day.wind_speed),
+              icon: this.getIconName(day.weather[0].icon),
+              date: this.convertDate(day.dt),
+            };
+          });
+
+          this.loaded = true;
         }
       } catch (error) {
         console.log(error.message)
@@ -145,7 +154,7 @@ export default {
           console.log(data.error_message)
         } else {
           // set current city
-          this.focusedWeather.city = data.results[0].address_components[0].short_name;
+          this.city = data.results[0].address_components[0].short_name;
         }
       } catch (error) {
         console.log(error.message);
@@ -311,5 +320,24 @@ body {
 
 .small-text{
   font-size: 18px;
+}
+
+.loader {
+  border: 5px solid #f3f3f3;
+  -webkit-animation: spin 1s linear infinite;
+  animation: spin 1s linear infinite;
+  border-top: 5px solid #555;
+  border-radius: 50%;
+  width: 200px;
+  height: 200px;
+  margin-top: 100px;
+  display: block;
+  margin-right: auto;
+  margin-left: auto;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
 }
 </style>
